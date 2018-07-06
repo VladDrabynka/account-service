a083ded4bfaa531689900fb19e0c314266a24093
Ivan Karpov <ivan.karpov@netcracker.com>
2018-07-06 15:03:59 +0300

    add new branch

--- account-service-impl/src/main/java/com/project/cloud/as/rest/AccountController.java
+++ account-service-impl/src/main/java/com/project/cloud/as/rest/AccountController.java
@@ -3,9 +3,9 @@ package com.project.cloud.as.rest;
 import com.project.cloud.as.model.Account;
 import com.project.cloud.as.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AccountController {
@@ -16,6 +16,16 @@ public class AccountController {
     @GetMapping("/{id}")
     public Account getAccountById(@PathVariable String id){
         return accountService.getAccountById(id);
+    }
+
+    @GetMapping("/")
+    public List<Account> getAllAccount(){
+        return accountService.getAllAccounts();
+    }
+
+    @PostMapping("/account")
+    public void createAccount(@RequestBody Account account){
+        accountService.createAccount(account);
     }
 
 }
--- account-service-impl/src/main/java/com/project/cloud/as/service/AccountService.java
+++ account-service-impl/src/main/java/com/project/cloud/as/service/AccountService.java
@@ -2,7 +2,11 @@ package com.project.cloud.as.service;
 
 import com.project.cloud.as.model.Account;
 
+import java.util.List;
+
 public interface AccountService {
     Account getAccountById(String id);
     Account createAccount(Account account);
+    Account updateAccount(Account account);
+    List<Account> getAllAccounts();
 }
